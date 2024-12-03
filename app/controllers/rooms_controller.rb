ede420259f2c32bb38c0bf8b7ef74bf9dfb902f7
ko-dai0102 <koudai.tanno@gmail.com>
2024-12-03 17:18:25 +0900

    コントローラーとルーティングの準備

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,25 @@
+class RoomsController < ApplicationController
+  def entrance
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'users/index'
+  root "rooms#entrance"
+
+  resources :rooms do
+    resources :reservations, only: [:new, :create]
+  end
+
+  resources :reservations, only: [:index, :show, :destroy]
 end
