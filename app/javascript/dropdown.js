47eb4b5f1c578b5e8ced3f1629a2e8fadd50129d
ko-dai0102 <koudai.tanno@gmail.com>
2024-12-04 12:35:40 +0900

    ドロップダウンメニューの完成

---
+++ app/javascript/dropdown.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dropDownStart = document.getElementById("dropdown-start");
+  const dropDownMenu = document.getElementById("dropdown-menu");
+
+  // トリガーをクリックしてメニューを開く/閉じる
+  dropDownStart.addEventListener("click", function (event) {
+    event.stopPropagation(); // クリックイベントの伝播を停止
+    if (dropDownMenu.style.display === "block") {
+      dropDownMenu.style.display = "none";
+    } else {
+      dropDownMenu.style.display = "block";
+    }
+  });
+
+  // メニュー外をクリックしたときに閉じる
+  document.addEventListener("click", function () {
+    dropDownMenu.style.display = "none";
+  });
+});
