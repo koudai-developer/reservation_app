b59708d221ce1735bc6ec85fc2c2f4ecc0c1adaa
ko-dai0102 <koudai.tanno@gmail.com>
2024-12-05 18:36:49 +0900

    施設情報の登録・参照・編集・削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,25 +1,59 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_room, only: [:show, :edit, :update, :destroy]
+
   def entrance
   end
 
   def index
-  end
-
-  def new
-  end
-
-  def create
+    if params[:query].present?
+      @rooms = Room.where("name LIKE ? OR detail LIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
+    elsif params[:area].present?
+      @rooms = Room.where("address LIKE ?", "%#{params[:area]}%")
+    else
+      @rooms = Room.all
+    end
   end
 
   def show
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      redirect_to rooms_path, notice: "施設が登録されました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @room.update(room_params)
+      redirect_to @room, notice: "施設情報が更新されました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @room.destroy
+    redirect_to rooms_path, notice: "施設が削除されました。"
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :description, :price, :address, :image)
   end
 end
